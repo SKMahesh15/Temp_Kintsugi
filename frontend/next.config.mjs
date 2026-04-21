8f94df26bb71c977b58e0bd635fb9c5cf78f3f42
summi-rahman <tahseenanwer100@gmail.com>
2026-04-21 22:20:42 +0530

    Adding Home Page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  devIndicators: {
+    appIsrStatus: false, 
+  },
 };
 
 export default nextConfig;
